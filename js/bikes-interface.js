311ac75f949edd2daef7fc6fbf7d6422b9673aa7
Guest User <Guest@Epicodus-7E.local>
2017-09-13 16:52:18 -0700

    got promises in

--- js/bikes-interface.js
+++ js/bikes-interface.js
@@ -1,40 +1,45 @@
-// import {Bike} from './../js/bikes.js'
+import {Bike} from './../js/bikes.js'
 
 $(document).ready(function() {
-  $('#bikeForm').submit(function() {
-    // let bike = new Bike ("Trek", "Blue", 97206);
+  $('#bikeForm').submit(function(event) {
+    event.preventDefault();
     let location = $('#location').val();
+    let bike = new Bike ("Trek", "Blue", 97206);
     let manufacturer = $('#manufacturer').val();
     let color = $('#color').val();
     let distance = $('#distance').val();
     let url = `https://bikeindex.org:443/api/v3/search?page=1&per_page=25&manufacturer=${manufacturer}&frame-colors=${color}&location=${location}&distance=${distance}&stolenness=proximity`;
 
-
-
-    debugger;
-    $.ajax({
-      url: url,
-      dataType: 'json',
-      type: 'GET',
-      success: function(data) {
-        console.log("success");
-      },
-      error: function() {
-        console.log('error');
-      }
+    bike.getAPIData(url,function(simpleBikeArray){
+      console.log(simpleBikeArray);
+      simpleBikeArray.forEach(function(bike) {
+        $(".results").append('<strong>Model: ' + bike.model + '</strong><br>');
+        $(".results").append('Year: ' + bike.year + '<br>');
+        $(".results").append('Serial #: ' + bike.serial + '<br>');
+        $(".results").append('Date Stolen: ' + bike.dateStolen + '<br>');
+        $(".results").append('Location Stolen: ' + bike.locationStolen + '<br><br>');
+      });
     });
 
 
 
-    // $.getJSON(url,function(data) {
-    //   debugger;
-    //   alert("test");
-    //   // let items = [];
-    //   // $.each(data, function())
-    //   let test = data.bikes[3].title;
-    //   debugger;
-    //   console.log(data.bikes[3].title);
+
+
+    // $.getJSON(url,function(data.bikes) {
+    //   console.log(data.bikes);
+    //   $.each(data, function() {
+    //     console.log(data.bikes.title)
+    //     // let model = data.bikes.;
+    //     // let serial = ;
+    //     // let year = ;
+    //     // let locationStolen = ;
+    //     // let bike[i]
+    //     // let test = data.bikes[3].title;
+    //     // debugger;
+    //     // console.log(data.bikes[3].title);
+    //   });
     // });
+
 
 
   });
--- js/bikes.js
+++ js/bikes.js
@@ -1,20 +1,50 @@
-// export class Bike {
-//   constructor(manufacturer,color,location) {
-//     this.manufacturer = manufacturer;
-//     this.color = color;
-//     this.location = location;
-//   }
-//
-//   getAPIData(url,fn) {
-//     $.ajax({
-//       url: url,
-//       dataType: 'JSON',
-//       type: 'GET',
-//       success: function(data) {
-//         let bikeParameters = [];
-//         let
-//         console.log(data);
-//       }
-//     });
-//   }
-// }
+export class Bike {
+  constructor(manufacturer,color,location) {
+    this.manufacturer = manufacturer;
+    this.color = color;
+    this.location = location;
+  }
+
+  getAPIData(url,fn) {
+    $.ajax({
+      data: {
+        format: 'json'
+      },
+      url: url,
+      type: 'GET',
+      success: (response) => {
+        let bikesArray = response.bikes;
+        let simpleBikeArray = this.parseBikeArray(bikesArray);
+        fn(simpleBikeArray);
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+               alert('An error occurred... Look at the console (F12 or Ctrl+Shift+I, Console tab) for more information!');
+
+               $('#result').html('<p>status code: '+jqXHR.status+'</p><p>errorThrown: ' + errorThrown + '</p><p>jqXHR.responseText:</p><div>'+jqXHR.responseText + '</div>');
+               console.log('jqXHR:');
+               console.log(jqXHR);
+               console.log('textStatus:');
+               console.log(textStatus);
+               console.log('errorThrown:');
+               console.log(errorThrown);
+           }
+    });
+  }
+
+  parseBikeArray(bikesArray) {
+    let simpleBikeArray = [];
+    bikesArray.forEach(function(bike){
+      let newBike = { model          : bike.frame_model,
+                      year           : bike.year,
+                      serial         : bike.serial,
+                      dateStolen     : moment.unix(bike.date_stolen).format("MM-DD-YYYY"),
+                      locationStolen : bike.stolen_location
+                    };
+      simpleBikeArray.push(newBike);
+    });
+    console.log("inside parseBikeArray: " + simpleBikeArray);
+    return simpleBikeArray;
+  }
+
+
+}
